36dd415bcd42cf326298c625cd2b416fe565e1ce
impleex <koshkindany@yandex.ru>
2025-01-09 04:07:00 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from .views import ( PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet )
+from .views import (PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet)
 
 router = SimpleRouter()
 router.register('posts', PostViewSet, basename='post')
-router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
-basename='comment')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 router.register('groups', GroupViewSet, basename='group')
 router.register('follow', FollowViewSet, basename='follow')
 urlpatterns = [
